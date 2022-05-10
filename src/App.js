17b9c99912205e3615d14c1b022a310ac4f5a456
unknown <hanmajsh1@gmail.com>
2022-05-10 10:17:38 +0900

    아이콘 수정 및 배치

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ function App() {
   return (
     <>
       <form>
-        <button>
+        {/*로그인*/}
+        <button className="Login_Button">
           <img src="login.png" className='Login' alt="로그인"/>
-           </button>
+        </button>
+        {/*기업순위*/}
         <button className="TOP_Button">
           <img src="arrow_right.png" className="TOP" alt="기업순위"/>
         </button>
       </form>
-
-      <div className="App-header">
-        {/* 로고이미지 */}
-        <img src="kimleejung_minilogo.png" className="App-logo" alt='logo'></img>
-        {/*로고명*/}
-        <img src="kimleejung_letterlogo.png" className="App-size" alt='logoName'></img>
-      </div>
-      &nbsp;&nbsp;
-      <div className="App-search">
-      {/*검색창*/}
-      <b>
-        <from>
-          <input type="search" className="App-Search" placeholder="기업명을 입력하시오." id="Enterprise" />
-          &nbsp;
-          {/* 검색버튼 */}
-          <button>
-            <img src="button.png" className="App-button" alt='버튼 이미지' />
-          </button>
-        </from>
-      </b>
-      </div>
+      <form className="Main">
+          <div className="App-header">
+            {/* 로고이미지 */}
+            <img src="kimleejung_minilogo.png" className="App-logo" alt='logo'></img>
+            {/*로고명*/}
+            <img src="kimleejung_letterlogo.png" className="App-size" alt='logoName'></img>
+          </div>
+          &nbsp;&nbsp;
+          <div className="App-search">
+          {/*검색창*/}
+            <b>
+              <from>
+                <input type="search" className="App-Search" placeholder="기업명을 입력하시오." id="Enterprise" />
+                &nbsp;
+                {/* 검색버튼 */}
+                <button className="search_button">
+                  <img src="button.png" className="App-button" alt='버튼 이미지' />
+                </button>
+              </from>
+            </b>
+          </div>
+        </form>
     </>
   );
 }
