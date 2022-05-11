17a91cfdb3a33168bb5b739bc83833da81f5f970
GWANHUN <tksxk6686@naver.com>
2022-05-11 22:06:08 +0900

    정리

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
+import {useState} from 'react'
 import './App.css';
-import React from 'react';
+import React, { Children } from 'react';
+
+
 
 function App() {
   const onLogin = (e) => {
-    alert('로그인')
+    alert('로그인 페이지로 이동합니다.')
   }
+
+  const [isNav,setNav]=useState(false)
   const onSide = (e) => {
-    alert('기업순위')
-  }
+      e.preventDefault();
+      setNav((prev) => !prev)
+    }
 
   return (
     <>
-      <form onSubmit={onLogin}>
-        {/*로그인*/}
-        <button className="Login_Button">
-          <img src="login.png" className='Login' alt="로그인"/>
-        </button>
-      </form>
+    <form onSubmit={onLogin}>
+      {/* 로그인 */}
+      <button className="Login_Button">
+      <img src="login.png" className='Login' alt="로그인"/>
+      </button>
+    </form>
 
-      <form onSubmit={onSide}>
-        {/*기업순위*/}
-        <button className="TOP_Button">
-          <img src="arrow_right.png" className="TOP" alt="기업순위"/>
-        </button>
-      </form>
-      
-      <form className="Main">
-          <div className="App-header">
-            {/* 로고이미지 */}
-            <img src="kimleejung_minilogo.png" className="App-logo" alt='logo'></img>
-            {/*로고명*/}
-            <img src="kimleejung_letterlogo.png" className="App-size" alt='logoName'></img>
-          </div>
-          &nbsp;&nbsp;
-          <div className="App-search">
-          {/*검색창*/}
-            <b>
-              <from>
-                <input type="search" className="App-Search" placeholder="기업명을 입력하시오." id="Enterprise" />
-                &nbsp;
-                {/* 검색버튼 */}
-                <button className="search_button">
-                  <img src="button.png" className="App-button" alt='버튼 이미지' />
-                </button>
-              </from>
-            </b>
-          </div>
+    {/* 기업순위 */}
+    <form onSubmit={onSide}>
+          <button className="TOP_Button">
+            <img src="arrow_push.png" className="TOP" alt="기업순위" />
+          </button>
         </form>
-    </>
-  );
-}
 
+    <div className='nav' style={isNav?{display:"block"}:{display:'none'}}>
+        <form onSubmit={onSide}>
+          <button className="Side_Button">
+            <img src="arrow_pull.png" className="TOP" alt="기업순위" />
+          </button>
+        </form> 
+      </div>
+
+    <form>
+      {/* 로고 이미지 */}
+      <img src="kimleejung_minilogo.png" className="App-logo" alt='logo'/>
+      {/* 로고명 */}
+      <img src="kimleejung_letterlogo.png" className="App-size" alr='logoName'/>
+      {/* 검색창 */}
+      <input type="search" className="App-Search" placeholder="기업명을 입력하시오." id="Enterprise" />
+      {/* 검색버튼 */}
+      <button className="search_button">
+      <img src="button.png" className="App-button" alt='버튼 이미지'/>
+      </button>  
+</form>
+
+</>
+  )
+}
 
 export default App;
